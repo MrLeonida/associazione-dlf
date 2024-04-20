d4e795424a4101e763f7ca86bb748431ff5922a9
MrLeonida <matteo.vistosi@gmail.com>
2024-04-20 21:35:51 +0200

    Dev commit.

--- src/components/promo.js
+++ src/components/promo.js
@@ -17,7 +17,7 @@ export default async function Promo() {
       <div className="flex items-center justify-center gap-x-6 bg-teal-800 px-6 py-2.5 sm:px-3.5">
         <p className="text-sm leading-6 text-white">
             <FontAwesomeIcon icon={faBells} className="fa-lg mr-1.5" />
-            <span className="font-semibold">Promozione in corso! </span>
+            <span className="font-semibold">Promo in corso! </span>
             {promos.map((promo) => (
               <a key={promo.fieldData._idNews} href={promo.fieldData.ctWebLink} className="underline underline-offset-2">
               {promo.fieldData.title} <span aria-hidden="true">&rarr;</span>
