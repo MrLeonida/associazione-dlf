875d886865304015d23d47b263186690bfed984a
MrLeonida <matteo.vistosi@gmail.com>
2024-12-06 09:44:17 +0100

    Dev commit.

--- src/app/lib/firestoreNewsActual.js
+++ src/app/lib/firestoreNewsActual.js
@@ -5,23 +5,55 @@ export async function fetchNewsActualFromFirestore() {
     try {
         const newsRef = collection(db, "news");
         const now = Timestamp.now();
-        const q = query(newsRef,
-                        where("category", "==", "Promo"),
-                        where("promo", "==", true),
-                        where("active", "==", true),
-                        where("timestamPromoFrom", "<=", now),
-                        where("timestampPromoTo", ">=", now),
-                        orderBy("timestamPromoFrom", "asc"),
-                        orderBy("timestampPromoTo", "asc"),
-                        limit(1));
-    
-        const querySnapshot = await getDocs(q);
-        const posts = querySnapshot.docs.map(doc => ({
-            id: doc.id, 
-            fieldData: doc.data()
-        }));
-    
-        return posts;
+
+        // Query per "Promo"
+        const promoQuery = query(newsRef,
+            where("category", "==", "Promo"),
+            where("promo", "==", true),
+            where("active", "==", true),
+            where("timestamPromoFrom", "<=", now),
+            where("timestampPromoTo", ">=", now),
+            orderBy("timestamPromoFrom", "asc"),
+            orderBy("timestampPromoTo", "asc"),
+            limit(1)
+        );
+
+        // Esegui la query per "Promo"
+        const promoSnapshot = await getDocs(promoQuery);
+
+        // Se esistono risultati per "Promo", restituiscili
+        if (!promoSnapshot.empty) {
+            const promoPosts = promoSnapshot.docs.map(doc => ({
+                id: doc.id,
+                fieldData: doc.data()
+            }));
+            return promoPosts;
+        }
+
+        // Se non ci sono risultati per "Promo", esegui la query per "Evento"
+        const eventQuery = query(newsRef,
+            where("category", "==", "Evento"),
+            where("active", "==", true),
+            where("timestamPromoFrom", "<=", now),
+            where("timestampPromoTo", ">=", now),
+            orderBy("timestamPromoFrom", "asc"),
+            orderBy("timestampPromoTo", "asc"),
+            limit(1)
+        );
+
+        const eventSnapshot = await getDocs(eventQuery);
+
+        // Se esistono risultati per "Evento", restituiscili
+        if (!eventSnapshot.empty) {
+            const eventPosts = eventSnapshot.docs.map(doc => ({
+                id: doc.id,
+                fieldData: doc.data()
+            }));
+            return eventPosts;
+        }
+
+        // Se non ci sono risultati in nessuna delle due categorie, restituisci un array vuoto
+        return [];
     } catch (error) {
         console.error('Failed to fetch data from Firestore:', error);
         return null;
